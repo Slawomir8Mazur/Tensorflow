8e621006dbfcf1efce95e3a4533d528bf17fc189
Slav <slawomir8mazur@gmail.com>
2019-02-18 14:47:39 +0100

    binary classification of text file

--- text_calssification.py
+++ text_calssification.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function
-
+'''
+example of binary classyfication of movie reviews, classificating as positive or negative feedback
+'''
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
@@ -9,7 +11,7 @@ imdb = keras.datasets.imdb
 (train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
 
 ''' Data exploration'''
-if True:
+if False:
     print("Training entries: {}, labels: {}".format(len(train_data), len(train_labels)))
     print(train_data[0], train_labels[0], sep='\n')
     print(len(train_data[0]), len(train_data[1]))
@@ -27,3 +29,89 @@ def decode_review(text):
     return ' '.join([reverse_word_index.get(i, '?') for i in text])
 
 print(decode_review(train_data[0]))
+
+''' Data preparation'''
+''' parsing data to size (for train_data): 
+        (2D_numpy_array, 
+        np.int64, 
+        (num_of_samples<25'000>, num_of_words<set 256 as top, more words shall be trunkated>)
+        )'''
+train_data = keras.preprocessing.sequence.pad_sequences(train_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data,
+                                                        value=word_index["<PAD>"],
+                                                        padding='post',
+                                                        maxlen=256)
+#print(train_data[0])
+
+''' Model building
+Input data: array of word-indices
+Output: Labels to predict: 0 or 1 (like or dislike
+'''
+vocab_size = 10000
+
+model = keras.Sequential()
+model.add(keras.layers.Embedding(vocab_size, 16))
+model.add(keras.layers.GlobalAveragePooling1D())
+model.add(keras.layers.Dense(16,activation=tf.nn.relu))
+model.add(keras.layers.Dense(1,activation=tf.nn.sigmoid))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+''' Validation setting'''
+x_val = train_data[:10000]
+partial_x_train = train_data[10000:]
+
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+''' Training the model'''
+history = model.fit(partial_x_train,
+                    partial_y_train,
+                    epochs=40,
+                    batch_size=512,
+                    validation_data=(x_val, y_val),
+                    verbose=1)
+
+''' Evaluation'''
+results = model.evaluate(test_data, test_labels)
+print(results)
+
+''' Graphical analisys'''
+if True:
+    history_dict = history.history
+    print(history_dict.keys())
+
+    import matplotlib.pyplot as plt
+
+    acc = history_dict['acc']
+    val_acc = history_dict['val_acc']
+    loss = history_dict['loss']
+    val_loss = history_dict['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    plt.clf()
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    plt.show()
