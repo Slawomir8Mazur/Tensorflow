bc86842aad92ad12e9f8260bdc474ecfe13cf812
Slav <slawomir8mazur@gmail.com>
2019-02-20 12:14:16 +0100

    continuation of regression example

---
+++ Regression_example.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+import pathlib
+
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow import layers
+
+dataset_path = keras.utils.get_file("auto-mpg.data", "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+print(dataset_path)
