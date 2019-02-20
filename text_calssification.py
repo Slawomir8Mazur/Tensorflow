5a7b58feed924f3abc72543b31b1a9d7ba13900b
Slav <slawomir8mazur@gmail.com>
2019-02-20 12:12:00 +0100

    binary classification of text file

--- text_calssification.py
+++ text_calssification.py
@@ -17,18 +17,19 @@ if False:
     print(len(train_data[0]), len(train_data[1]))
 
 ''' Convert to words'''
-word_index = imdb.get_word_index()
-word_index = {k:(v+3) for k,v in word_index.items()}
-word_index["<PAD>"] = 0
-word_index["<START>"] = 1
-word_index["<UNK>"] = 2
-word_index["<UNUSED>"] = 3
-reverse_word_index = dict([(value, key) for (key,value) in word_index.items()])
+if False:
+    word_index = imdb.get_word_index()
+    word_index = {k:(v+3) for k,v in word_index.items()}
+    word_index["<PAD>"] = 0
+    word_index["<START>"] = 1
+    word_index["<UNK>"] = 2
+    word_index["<UNUSED>"] = 3
+    reverse_word_index = dict([(value, key) for (key,value) in word_index.items()])
 
-def decode_review(text):
-    return ' '.join([reverse_word_index.get(i, '?') for i in text])
+    def decode_review(text):
+        return ' '.join([reverse_word_index.get(i, '?') for i in text])
 
-print(decode_review(train_data[0]))
+    print(decode_review(train_data[0]))
 
 ''' Data preparation'''
 ''' parsing data to size (for train_data): 
