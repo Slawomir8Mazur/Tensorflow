cfe11d432733987ef88dcf17d3201714c634245b
Slav <slawomir8mazur@gmail.com>
2019-02-22 18:54:34 +0100

    regression example using neural networks

--- overfitting_and_underfitting.py
+++ overfitting_and_underfitting.py
@@ -115,7 +115,7 @@ if False:
     plt.show()
 
 ''' Regularization'''
-if True:
+if False:
     l2_model = keras.Sequential([
         keras.layers.Dense(16, kernel_regularizer=keras.regularizers.l2(0.001),
                            activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
@@ -140,3 +140,25 @@ if True:
     plt.show()
 
 ''' Dropout'''
+if True:
+    dpt_model = keras.Sequential([
+        keras.layers.Dense(16, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(16, activation=tf.nn.relu),
+        keras.layers.Dropout(0.5),
+        keras.layers.Dense(1, activation=tf.nn.sigmoid)
+    ])
+
+    dpt_model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy', 'binary_crossentropy'])
+
+    dpt_model_history = dpt_model.fit(train_data,
+                                      train_labels,
+                                      epochs=10,
+                                      validation_data=(test_data, test_labels),
+                                      verbose=2)
+
+    plot_history([('baseline', baseline_history),
+                  ('dpt', dpt_model_history)])
+    plt.show()
